2a0e345bc907e2aee00817ea4170f848d120d1c6
Ryan <ryansilvadeoliveira53@gmail.com>
2025-11-09 03:44:21 -0300

    conseto código

--- docs/javascript/script.js
+++ docs/javascript/script.js
@@ -2,7 +2,7 @@ const openButtons = document.querySelectorAll('.open-modal');
 
 openButtons.forEach (button => {
     button.addEventListener('click', () =>{
-        const modalId = button.getAttribute('data-model');
+        const modalId = button.getAttribute('data-modal');
         const modal = document.getElementById(modalId);
 
         modal.showModal();
